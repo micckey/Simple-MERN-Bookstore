6dbeeea8ab52ae6599ccb5acf7b6bb2c62401659
micckey <83111475+micckey@users.noreply.github.com>
2023-12-04 19:02:23 +0300

    Second commit

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://owen:ichimaruGin@cluster0.g4w7k5w.mongodb.net/books-collection?retryWrites=true&w=majority';
+export const mongoDBURL =
+  "mongodb+srv://owen:ichimaruGin@cluster0.g4w7k5w.mongodb.net/books-collection?retryWrites=true&w=majority";
